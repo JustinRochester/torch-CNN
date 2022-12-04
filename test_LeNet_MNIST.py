4dac10edc6dd98e70505d90d753ef8c5a1564480
JustinRochester <60056121+JustinRochester@users.noreply.github.com>
2022-12-04 14:57:23 +0800

    upload data and train code

---
+++ test_LeNet_MNIST.py
@@ -0,0 +1,13 @@
+from CNNs.LeNet_MNIST.LeNet import LeNet
+
+
+if __name__ == '__main__':
+    nn = LeNet(
+        learning_rate=1e-1
+    )
+    nn.work(
+        epoch=10,
+        batch_size=4096,
+        run_size=1024,
+        version=5
+    )
