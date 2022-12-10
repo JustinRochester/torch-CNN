fixed the batch normalization

CNN-practice/CNNs/CNN/Base/NeuralVariable.py
@@ -6,7 +6,7 @@ class NeuralVariable(NeuralData):
     def __init__(self, shape=(1,), initial_mu=0, initial_std=1):
         self.shape = shape
         self.value = np.random.normal(initial_mu, initial_std, shape)
-        self.grad = np.zeros(shape)
+        self.grad = np.empty(shape)
 
     def load_data(self, data_iter):
         self.value = next(data_iter)

CNN-practice/CNNs/CNN/Layers/Activation.py
@@ -11,12 +11,12 @@ class Activation(Layer):
         self.input_size = self.output_size = input_size
         self.activation = activation_dict[activation_name]
 
-    def predict_forward(self, input):
-        return self.activation.forward(input)
+    def predict_forward(self, input_value):
+        return self.activation.forward(input_value)
 
-    def forward(self, input):
-        self.input = input
-        self.output = self.predict_forward(input)
+    def forward(self, input_value):
+        self.input = input_value
+        self.output = self.predict_forward(input_value)
         return np.copy(self.output)
 
     def backward(self, output_grad):

CNN-practice/CNNs/CNN/Layers/BatchNormalization.py
@@ -1,30 +1,27 @@
 from ..GPU_np import np
 from .Layer import Layer
 from ..Base.NeuralVariable import NeuralVariable
-from ..Base.AverageVariable import AverageVariable
 
 eps = 1e-9
 
 
 class BatchNormalization(Layer):
-    def __init__(self, input_size=(1,)):
+    def __init__(self, input_size=(1,), rho=0.9):
         super().__init__()
         self.input_size = input_size
         self.output_size = input_size
         self.channel_size = input_size[0]
-        self.parameter_shape = (1, self.channel_size) + (1,) * len(input_size[1:])
+        self.parameter_shape = (1, self.channel_size, 1)
         self.input_norm = None
+        self.rho = rho
 
-        self.batch_mu = np.zeros(self.parameter_shape)
-        self.mu = AverageVariable(self.parameter_shape)
+        self.sample_mu = np.zeros(self.parameter_shape)
+        self.sample_sigma2 = np.ones(self.parameter_shape)
+        self.sample_sigma = np.sqrt(self.sample_sigma2 + eps)
 
-        self.batch_sigma2 = np.ones(self.parameter_shape)
-        self.sigma2 = AverageVariable(self.parameter_shape)
-
-        self.batch_sigma = np.sqrt(self.batch_sigma2 + eps)
-
-        self.slope = np.ones(self.parameter_shape)
-        self.bias = np.zeros(self.parameter_shape)
+        self.running_mu = np.zeros(self.parameter_shape)
+        self.running_sigma2 = np.ones(self.parameter_shape)
+        self.running_sigma = np.sqrt(self.running_sigma2 + eps)
 
         self.gamma = NeuralVariable(
             shape=self.parameter_shape,
@@ -41,66 +38,47 @@ class BatchNormalization(Layer):
             "beta": self.beta,
         }
 
-    def predict_forward(self, input_value):
-        return self.slope * input_value + self.bias
+    def predict_forward(self, input):
+        n, c = input.shape[:2]
+        input = (input.reshape(n, c, -1) - self.running_mu) / self.running_sigma
+        output = input * self.gamma.value + self.beta.value
+        return output.reshape((n,) + self.output_size)
 
-    def forward(self, input_value):
-        n = input_value.shape[0]
-        mu = np.average(input_value.reshape((n, self.channel_size, -1)), axis=2)
-        mu = np.average(mu, axis=0).reshape(self.batch_mu.shape)
-        sigma2 = np.average(np.square(input_value - mu).reshape((n, self.channel_size, -1)), axis=2)
-        sigma2 = np.average(sigma2, axis=0).reshape(self.batch_sigma2.shape)
+    def forward(self, input):
+        n, c = input.shape[:2]
+        input = input.reshape((n, c, -1))
+        average_size = n * input.shape[2]
 
-        self.batch_mu = mu
-        self.batch_sigma2 = sigma2
-        self.batch_sigma = np.sqrt(self.batch_sigma2 + eps)
+        self.sample_mu = np.einsum('ijk->j', input) / average_size
+        self.sample_mu = self.sample_mu.reshape(self.parameter_shape)
+        self.sample_sigma2 = np.einsum('ijk->j', np.square(input-self.sample_mu)) / average_size
+        self.sample_sigma2 = self.sample_sigma2.reshape(self.parameter_shape)
+        self.sample_sigma = np.sqrt(self.sample_sigma2 + eps)
 
-        self.mu += self.batch_mu
-        self.sigma2 += self.batch_sigma2
+        self.running_mu = self.running_mu * (1 - self.rho) + self.sample_mu * self.rho
+        self.running_sigma2 = self.running_sigma2 * (1 - self.rho) + self.sample_sigma2 * self.rho
+        self.running_sigma = np.sqrt(self.running_sigma2 + eps)
 
-        self.input_norm = (input_value - self.batch_mu) / self.batch_sigma
+        self.input_norm = (input - self.sample_mu) / self.sample_sigma
         output = self.input_norm * self.gamma.value + self.beta.value
-        return output
+        return output.reshape((n,) + self.output_size)
 
     def backward(self, output_grad):
-        n = output_grad.shape[0]
+        n, c = output_grad.shape[:2]
+        output_grad = output_grad.reshape((n, c, -1))
 
-        norm_grad = (output_grad * self.gamma.value).reshape((n, self.channel_size, -1))
-        self.input_norm = self.input_norm.reshape((n, self.channel_size, -1))
-        output_grad = output_grad.reshape((n, self.channel_size, -1))
-        self.gamma.grad += np.sum(
-            np.average(output_grad * self.input_norm, axis=2),
-            axis=0
-        ).reshape(self.parameter_shape)
-        self.beta.grad += np.sum(
-            np.average(output_grad, axis=2),
-            axis=0
-        ).reshape(self.parameter_shape)
+        gamma_grad = np.einsum('ijk->j', output_grad * self.input_norm) / output_grad.shape[2]
+        self.gamma.grad += gamma_grad.reshape(self.parameter_shape)
 
-        sigma_grad = -np.average(norm_grad * self.input_norm, axis=2) / 2
-        sigma_grad /= (self.batch_sigma2.reshape((1, self.channel_size)) + eps)
-        mu_grad = -np.average(norm_grad, axis=2) / self.batch_sigma.reshape((1, self.channel_size)) \
-                  - np.average(self.input_norm, axis=2) * sigma_grad * self.batch_sigma.reshape((1, self.channel_size)) * 2 / n
+        beta_grad = np.einsum('ijk->j', output_grad) / output_grad.shape[2]
+        self.beta.grad += beta_grad.reshape(self.parameter_shape)
 
-        sigma_grad = sigma_grad.reshape((n,) + self.parameter_shape[1:])
-        mu_grad = mu_grad.reshape((n,) + self.parameter_shape[1:])
-        norm_grad = norm_grad.reshape((n,) + self.input_size)
-        self.input_norm = self.input_norm.reshape((n,) + self.input_size)
+        norm_grad = output_grad * self.gamma.value
+        sigma2_grad = np.einsum('ijk->j', norm_grad * self.input_norm) / output_grad.shape[2]
+        sigma2_grad = -sigma2_grad.reshape(self.parameter_shape) / (2 * (self.sample_sigma2 + eps))
+        mu_grad = norm_grad / self.sample_sigma + 2 * self.sample_sigma * sigma2_grad * self.input_norm / n
+        mu_grad = - np.einsum('ijk->j', mu_grad).reshape(self.parameter_shape) / output_grad.shape[2]
 
-        input_grad = norm_grad / self.batch_sigma + (mu_grad + 2 * sigma_grad * self.batch_sigma * self.input_norm) / n
+        input_grad = norm_grad / self.sample_sigma + (mu_grad + 2 * sigma2_grad * self.sample_sigma * self.input_norm) / n
         self.input_norm = None
-
-        sigma = np.sqrt(self.sigma2.value + eps)
-        self.slope = self.gamma.value / sigma
-        self.bias = self.beta.value - self.gamma.value * self.mu.value / sigma
-        return input_grad
-
-    def get_data(self):
-        return self.mu.get_data() + self.sigma2.get_data()
-
-    def load_data(self, data_iter):
-        self.mu.load_data(data_iter)
-        self.sigma2.load_data(data_iter)
-        sigma = np.sqrt(self.sigma2.value + eps)
-        self.slope = self.gamma.value / sigma
-        self.bias = self.beta.value - self.gamma.value * self.mu.value / sigma
+        return input_grad.reshape((n,) + self.output_size)

CNN-practice/CNNs/CNN/Loss/CrossEntropySoftmax.py
@@ -3,12 +3,15 @@ from .LossFunction import LossFunction
 from ..Base.softmax import softmax
 
 
+eps = 1e-20
+
+
 class CrossEntropySoftmax(LossFunction):
     @staticmethod
     def loss(label, output, regular_loss):
         n = output.shape[0]
         label = label.reshape((n, -1))
         softmax_output = softmax(output).reshape((n, -1))
-        loss_value = - np.sum(label * np.log(softmax_output), axis=1) + regular_loss
+        loss_value = - np.sum(label * np.log(softmax_output + eps), axis=1) + regular_loss
         grad_output = np.sum(label, axis=1).reshape((n, 1)) * softmax_output - label
         return loss_value, grad_output.reshape((n, -1, 1))